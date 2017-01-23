a117af26c74d7022ac2f07a5f87e93b408389dcd
cyril-lemaire <cyril.lemaire@wanadoo.fr>
2017-01-23 18:46:46 +0100

    Partie projet

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ExamFinalBundle\ExamFinalBundle(),
+            new QuidditchBundle\QuidditchBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
